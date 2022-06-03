6b03bc5097a2d816fdc5b532551355e07a0c69f3
ryumtym <mtymryu@gunma-u.ac.jp>
2022-06-03 15:11:33 +0900

    Update
    Update

--- src/components/LaunchApp.vue
+++ src/components/LaunchApp.vue
@@ -8,20 +8,23 @@
   import FHIR from "fhirclient"
   export default {
     created(){
+      // smart launcher test
+      
       FHIR.oauth2.authorize({
-        client_id: "testid", //auth id
-        scope: "launch openid fhirUser patient/*.read",
+        client_id: "my_web_app",
+        scope: "patient/*.read launch/patient",
         redirectUri: "http://localhost:8080", //vue
-        iss:"http://localhost:3000/4_0_0/" //fhirServer
+        iss:"https://launch.smarthealthit.org/v/r4/sim/eyJoIjoiMSIsImoiOiIxIn0/fhir" //fhirServer
       });
+
+      // FHIR.oauth2.authorize({
+      //   client_id: "testid", //auth id
+      //   scope: "launch openid fhirUser patient/*.read",
+      //   redirectUri: "http://localhost:8080", //vue
+      //   iss:"http://localhost:3000/4_0_0/" //fhirServer
+      // });
     }
   }
 </script>
 
-      // smart launcher test
-      // FHIR.oauth2.authorize({
-      //   client_id: "my_web_app",
-      //   scope: "patient/*.read launch/patient",
-      //   redirectUri: "http://localhost:8080", //vue
-      //   iss:"https://launch.smarthealthit.org/v/r4/sim/eyJoIjoiMSIsImoiOiIxIn0/fhir" //fhirServer
-      // });
+
